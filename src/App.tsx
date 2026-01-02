95ebc9539bd9da700148598d25bdcb594b52814e
baldorjzagdaa-source <baldorjzagdaa@gmail.com>
2026-01-03 05:39:22 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>EduClub.mn</h1>
+      <p>Vite + React + TypeScript амжилттай ажиллаж байна 🎉</p>
+    </div>
+  );
+}
+
+export default App;
