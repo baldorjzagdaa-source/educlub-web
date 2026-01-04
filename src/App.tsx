f540c1e2832aa97bc3de73c6a41085a9b8a2f424
baldorjzagdaa-source <baldorjzagdaa@gmail.com>
2026-01-04 19:49:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>EduClub.mn</h1>
-      <p>Vite + React + TypeScript амжилттай ажиллаж байна 🎉</p>
+      <h1>EduClub.mn 🚀</h1>
+
+      <p>
+        Vite + React + TypeScript амжилттай ажиллаж байна 🎉
+      </p>
     </div>
   );
 }
