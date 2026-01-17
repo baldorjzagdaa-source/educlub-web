1d1a7d3a5c9bbbd7a10537385110b35e194e5146
Baldorj Zagdaa <Baldorjzagdaa@gmail.com>
2026-01-17 21:26:13 +0800

    fix: correct router nesting with MainLayout and Outlet

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,18 +2,11 @@ import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Header */}
-      <header>
-        EduClub
-      </header>
-
-      {/* Page content */}
-      <main className="flex-1">
+    <div className="layout">
+      <main>
         <Outlet />
       </main>
 
-      {/* Footer */}
       <footer className="footer">
         <div className="footer-content">
           © EduClub.mn
